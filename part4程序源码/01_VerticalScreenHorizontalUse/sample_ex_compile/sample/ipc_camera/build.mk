02ca2911d3be2c2b9828caf610e31a61d6bb9607
Ubuntu <t927456729@gmail.com>
2025-12-10 05:02:28 -0500

    Build Hello Tina MPP!

--- part4程序源码/01_VerticalScreenHorizontalUse/sample_ex_compile/sample/ipc_camera/build.mk
+++ part4程序源码/01_VerticalScreenHorizontalUse/sample_ex_compile/sample/ipc_camera/build.mk
@@ -21,14 +21,14 @@ TARGET_PATH :=$(call my-dir)
 TOP_PATH := $(TARGET_PATH)
 
 # ============================================================================
-# start gcc sample_virvi2venc.c
+# start gcc main.c
 #########################################
 include $(ENV_CLEAR)
+TARGET_SRC :=  \
+    $(TARGET_PATH)/app/main.c \
 
-include $(TARGET_TOP)/middleware/config/mpp_config.mk
-
-TARGET_SRC := sample_virvi2venc.c
 TARGET_INC := \
+    $(TARGET_PATH)/include \
     $(TARGET_TOP)/system/include \
     $(TARGET_TOP)/middleware/include/utils \
     $(TARGET_TOP)/middleware/include/media \
@@ -47,85 +47,26 @@ TARGET_INC := \
     $(TARGET_TOP)/middleware/sample/configfileparser \
 
 TARGET_SHARED_LIB := \
-    librt \
     libpthread \
     liblog \
-    libion \
-    libhwdisplay \
-    libasound \
-    libsample_confparser \
-    libcdx_base \
-    libcdx_parser \
-    libcdx_stream \
-    libcdx_common \
-    libcutils
-
-TARGET_STATIC_LIB := \
-    libaw_mpp \
-    libISP \
-    libisp_ae \
-    libisp_af \
-    libisp_afs \
-    libisp_awb \
-    libisp_base \
-    libisp_gtm \
-    libisp_iso \
-    libisp_math \
-    libisp_md \
-    libisp_pltm \
-    libisp_rolloff \
-    libmatrix \
-    libiniparser \
-    libisp_ini \
-    libisp_dev \
-    libmedia_utils \
-    libmuxers \
-    libmp4_muxer \
-    libraw_muxer \
-    libmpeg2ts_muxer \
-    libaac_muxer \
-    libmp3_muxer \
-    libffavutil \
-    libFsWriter \
-    libcedarxstream \
-    libcedarxdemuxer \
-    libadecoder \
-    libcedarx_aencoder \
-    libaacenc \
-    libadpcmenc \
-    libg711enc \
-    libg726enc \
-    libmp3enc \
-    libvdecoder \
-    libvideoengine \
-    libawh264 \
-    libawh265 \
-    libawmjpegplus \
-    libvencoder \
-    libvenc_codec \
-    libvenc_base \
     libMemAdapter \
-    libVE \
-    libcdc_base \
+    libvdecoder \
+    libmedia_utils \
+    libmedia_mpp \
+    libmpp_vi \
+    libmpp_vo \
+    libmpp_isp \
+    libmpp_ise \
+    libmpp_component \
+    libsample_confparser \
+    
+TARGET_STATIC_LIB :=
 
-ifeq ($(MPPCFG_ISE),Y)
-TARGET_STATIC_LIB += \
-    libise \
-    lib_ise_mo
-endif
+TARGET_CPPFLAGS += -fPIC -Wall -Wno-unused-but-set-variable
+TARGET_CFLAGS += -fPIC -Wall -Wno-unused-but-set-variable
 
-ifeq ($(MPPCFG_EIS),Y)
-TARGET_STATIC_LIB += \
-    libEIS \
-    lib_eis
-endif
+#TARGET_LDFLAGS += -static
 
-ifeq ($(MPPCFG_USE_KFC),Y)
-TARGET_STATIC_LIB += lib_hal
-endif
-
-TARGET_CFLAGS += -fPIC -Wall -Wno-unused-but-set-variable -ldl
-
-TARGET_MODULE := sample_virvi2venc
+TARGET_MODULE := ipc_camera
 
 include $(BUILD_BIN)
--- part4程序源码/01_VerticalScreenHorizontalUse/sample_ex_compile/sample/ipc_camera/build_static.mk
+++ part4程序源码/01_VerticalScreenHorizontalUse/sample_ex_compile/sample/ipc_camera/build_static.mk
@@ -21,11 +21,17 @@ TARGET_PATH :=$(call my-dir)
 TOP_PATH := $(TARGET_PATH)
 
 # ============================================================================
-# start gcc sample_vipp2venc.c
+# start gcc main.c
 #########################################
 include $(ENV_CLEAR)
-TARGET_SRC := sample_virvi2venc.c
+
+include $(TARGET_TOP)/middleware/config/mpp_config.mk
+
+TARGET_SRC := \
+    $(TARGET_PATH)/app/main.c
+
 TARGET_INC := \
+    $(TARGET_PATH)/include \
     $(TARGET_TOP)/system/include \
     $(TARGET_TOP)/middleware/include/utils \
     $(TARGET_TOP)/middleware/include/media \
@@ -44,26 +50,85 @@ TARGET_INC := \
     $(TARGET_TOP)/middleware/sample/configfileparser \
 
 TARGET_SHARED_LIB := \
+    librt \
     libpthread \
     liblog \
-    libMemAdapter \
-    libvdecoder \
-    libmedia_utils \
-    libmedia_mpp \
-    libmpp_vi \
-    libmpp_vo \
-    libmpp_isp \
-    libmpp_ise \
-    libmpp_component \
+    libion \
+    libhwdisplay \
+    libasound \
     libsample_confparser \
-    
-TARGET_STATIC_LIB :=
+    libcdx_base \
+    libcdx_parser \
+    libcdx_stream \
+    libcdx_common \
+    libcutils
 
-TARGET_CPPFLAGS += -fPIC -Wall -Wno-unused-but-set-variable
-TARGET_CFLAGS += -fPIC -Wall -Wno-unused-but-set-variable
+TARGET_STATIC_LIB := \
+    libaw_mpp \
+    libISP \
+    libisp_ae \
+    libisp_af \
+    libisp_afs \
+    libisp_awb \
+    libisp_base \
+    libisp_gtm \
+    libisp_iso \
+    libisp_math \
+    libisp_md \
+    libisp_pltm \
+    libisp_rolloff \
+    libmatrix \
+    libiniparser \
+    libisp_ini \
+    libisp_dev \
+    libmedia_utils \
+    libmuxers \
+    libmp4_muxer \
+    libraw_muxer \
+    libmpeg2ts_muxer \
+    libaac_muxer \
+    libmp3_muxer \
+    libffavutil \
+    libFsWriter \
+    libcedarxstream \
+    libcedarxdemuxer \
+    libadecoder \
+    libcedarx_aencoder \
+    libaacenc \
+    libadpcmenc \
+    libg711enc \
+    libg726enc \
+    libmp3enc \
+    libvdecoder \
+    libvideoengine \
+    libawh264 \
+    libawh265 \
+    libawmjpegplus \
+    libvencoder \
+    libvenc_codec \
+    libvenc_base \
+    libMemAdapter \
+    libVE \
+    libcdc_base \
 
-#TARGET_LDFLAGS += -static
+ifeq ($(MPPCFG_ISE),Y)
+TARGET_STATIC_LIB += \
+    libise \
+    lib_ise_mo
+endif
 
-TARGET_MODULE := sample_virvi2venc
+ifeq ($(MPPCFG_EIS),Y)
+TARGET_STATIC_LIB += \
+    libEIS \
+    lib_eis
+endif
+
+ifeq ($(MPPCFG_USE_KFC),Y)
+TARGET_STATIC_LIB += lib_hal
+endif
+
+TARGET_CFLAGS += -fPIC -Wall -Wno-unused-but-set-variable -ldl
+
+TARGET_MODULE := ipc_camera
 
 include $(BUILD_BIN)
