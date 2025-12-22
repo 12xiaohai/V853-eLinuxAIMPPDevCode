c0c221e5a41b120da909c7afff24edf990a1b9e1
Ubuntu <t927456729@gmail.com>
2025-12-22 07:53:30 -0500

    add .h in sample_g2d.h

--- part1程序源码/06_视频输入输出模块/6-10_使用G2D实现图像旋转缩放/sample_g2d/sample_g2d.h
+++ part1程序源码/06_视频输入输出模块/6-10_使用G2D实现图像旋转缩放/sample_g2d/sample_g2d.h
@@ -1,96 +1,94 @@
 #ifndef __SAMPLE_G2D_H__
-#define __SAMPLE_G2D_H__ 
+#define __SAMPLE_G2D_H__
 
 #include <plat_type.h>
+#include<mm_comm_sys.h>
+#include <media/mm_comm_vi.h>
+#include <utils/PIXEL_FORMAT_E_g2d_format_convert.h>
 
-#define FRAME_TO_BE_PROCESS     (5)
-#define MAX_FILE_PATH_LEN       (128)
-#define MAX_FILE_PATH_SIZE      (256)
 
-typedef struct SampleG2dCmdLineParam_s
-{
-    char mConfigFilePath[MAX_FILE_PATH_SIZE];
-}SampleG2dCmdLineParam; 
+#define FRAME_TO_BE_PROCESS (5)
+#define MAX_FILE_PATH_LEN (128)
+#define MAX_FILE_PATH_SIZE (256)
 
-typedef struct SampleG2dConfig_s
-{
-    PIXEL_FORMAT_E  mPicFormat; 
-    PIXEL_FORMAT_E  mDstPicFormat;
-    int mSrcWidth;
-    int mSrcHeight;
-    int mSrcRectX;
-    int mSrcRectY;
-    int mSrcRectW;
-    int mSrcRectH;
-    
-    int mDstRotate; //0, 90, 180, 270, clock-wise.
-    
-    int mDstWidth;
-    int mDstHeight;
-    int mDstRectX;
-    int mDstRectY;
-    int mDstRectW;
-    int mDstRectH;
-    
-    char flip_flag;
-    
-    char SrcFile[MAX_FILE_PATH_LEN];
-    char DstFile[MAX_FILE_PATH_LEN];
+typedef struct SampleG2dCmdLineParam_s {
+  char mConfigFilePath[MAX_FILE_PATH_SIZE];
+} SampleG2dCmdLineParam;
 
-    int g2d_mod;
+typedef struct SampleG2dConfig_s {
+  PIXEL_FORMAT_E mPicFormat;
+  PIXEL_FORMAT_E mDstPicFormat;
+  int mSrcWidth;
+  int mSrcHeight;
+  int mSrcRectX;
+  int mSrcRectY;
+  int mSrcRectW;
+  int mSrcRectH;
 
-    int g2d_bld_mod;
-}SampleG2dConfig; 
+  int mDstRotate; // 0, 90, 180, 270, clock-wise.
 
-typedef struct frm_info_s
-{
-    unsigned int frm_width;
-    unsigned int frm_height;
-    void *p_vir_addr[3];
-    void *p_phy_addr[3];
-}FRM_INFO; 
+  int mDstWidth;
+  int mDstHeight;
+  int mDstRectX;
+  int mDstRectY;
+  int mDstRectW;
+  int mDstRectH;
 
-typedef struct SampleG2dMixerTaskConfig
-{
-    PIXEL_FORMAT_E mInputYUVFormat;
-    PIXEL_FORMAT_E mInputRGBFormat;
-}SampleG2dMixerTaskConfig;
+  char flip_flag;
 
-typedef struct SampleG2dMixerTaskFrmInfo
-{
-    g2d_fmt_enh mSrcPicFormat;
-    int mSrcWidth;
-    int mSrcHeight;
-    g2d_fmt_enh mDstPicFormat;
-    int mDstWidth;
-    int mDstHeight;
-    void *mpSrcVirFrmAddr[3];
-    void *mpDstVirFrmAddr[3];
-}SampleG2dMixerTaskFrmInfo;
+  char SrcFile[MAX_FILE_PATH_LEN];
+  char DstFile[MAX_FILE_PATH_LEN];
 
-typedef struct SampleG2dMixerTaskContext
-{
-    int mG2dFd;
-    SampleG2dMixerTaskFrmInfo mYUVFrmInfo;
-    SampleG2dMixerTaskFrmInfo mRGBFrmInfo;
-    SampleG2dMixerTaskFrmInfo mFrmInfo[FRAME_TO_BE_PROCESS];
-    struct mixer_para stMixPara[FRAME_TO_BE_PROCESS];
-}SampleG2dMixerTaskContext;
+  int g2d_mod;
 
-typedef struct sample_g2d_ctx_s
-{ 
-    SampleG2dConfig mConfigPara; 
-    SampleG2dCmdLineParam mCmdLinePara;
-    SampleG2dMixerTaskContext mixertask;
+  int g2d_bld_mod;
+} SampleG2dConfig;
 
-    MPP_SYS_CONF_S mSysConf;
-    
-    FRM_INFO src_frm_info;
-    FRM_INFO dst_frm_info; 
-    
-    FILE * fd_in;
-    FILE * fd_out;
-    int mG2dFd;
-}SAMPLE_G2D_CTX;
+typedef struct frm_info_s {
+  unsigned int frm_width;
+  unsigned int frm_height;
+  void *p_vir_addr[3];
+  void *p_phy_addr[3];
+} FRM_INFO;
+
+typedef struct SampleG2dMixerTaskConfig {
+  PIXEL_FORMAT_E mInputYUVFormat;
+  PIXEL_FORMAT_E mInputRGBFormat;
+} SampleG2dMixerTaskConfig;
+
+typedef struct SampleG2dMixerTaskFrmInfo {
+  g2d_fmt_enh mSrcPicFormat;
+  int mSrcWidth;
+  int mSrcHeight;
+  g2d_fmt_enh mDstPicFormat;
+  int mDstWidth;
+  int mDstHeight;
+  void *mpSrcVirFrmAddr[3];
+  void *mpDstVirFrmAddr[3];
+} SampleG2dMixerTaskFrmInfo;
+
+typedef struct SampleG2dMixerTaskContext {
+  int mG2dFd;
+  SampleG2dMixerTaskFrmInfo mYUVFrmInfo;
+  SampleG2dMixerTaskFrmInfo mRGBFrmInfo;
+  SampleG2dMixerTaskFrmInfo mFrmInfo[FRAME_TO_BE_PROCESS];
+  struct mixer_para stMixPara[FRAME_TO_BE_PROCESS];
+} SampleG2dMixerTaskContext;
+
+typedef struct sample_g2d_ctx_s {
+  SampleG2dConfig mConfigPara;
+  SampleG2dCmdLineParam mCmdLinePara;
+  SampleG2dMixerTaskContext mixertask;
+
+  MPP_SYS_CONF_S mSysConf;
+
+  FRM_INFO src_frm_info;
+  FRM_INFO dst_frm_info;
+
+  FILE *fd_in;
+  FILE *fd_out;
+  int mG2dFd;
+} SAMPLE_G2D_CTX;
 
 #endif
+
